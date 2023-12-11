2ae4112a8bf43ea49848603080b5131e1bea94b6
absmugz <absmugz09@gmail.com>
2023-12-11 11:06:23 +0200

    Countries information

--- App.tsx
+++ App.tsx
@@ -131,7 +131,7 @@ function App(): React.JSX.Element {
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
-          <Section title="Sample Country">
+          <Section title="Countries Information">
           <FlatList
              data={c.countries}
               keyExtractor={item => item.code}
